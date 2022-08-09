0c82b49404887f8943c3b6bd06aa26fd272786a7
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-09 09:53:51 -0500

    commit tree 4020664730a6faa015f55da9f25b74cb4ae9d02f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(int)
+    counter = 0
+    while counter < int
+        puts "#{int} SECOND(S)!"
+        int -= 1  
+    end
+    "HAPPY NEW YEAR!"        
+end
+
+def countdown_with_sleep (int)
+    sleep(5)
+    countdown(int)
 end
